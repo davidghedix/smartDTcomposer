4440f85baf0159edd66cd0ba27e0c8004a3f54ca
David <davidghedix@hotmail.it>
2020-10-24 16:32:20 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+ThingsAPI = dict(
+
+    namespace = 'YOUR-NAMESPACE',
+
+    clientId = 'YOUR-CLIENT-ID',
+
+    clientSecret = 'YOUR-CLIENT-SECRET',
+
+    scope = 'YOUR-SCOPE'
+
+)
+
+
+
+PDDL = dict(
+
+    domainName = 'myDomain',
+
+    problemName = 'myProblem',
+
+    domainFile = 'myDomain.pddl',
+
+    problemFile = 'myProblem.pddl',
+
+    planFile = 'sas_plan'
+
+)
